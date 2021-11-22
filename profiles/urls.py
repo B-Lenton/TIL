2c9410e2cc6abac703ca6cc3a60a686a8864d67c
Ben Lenton <benlenton@gmail.com>
2021-11-22 15:49:55 +0000

    WIP: incomplete addition of REST framework

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
-from profiles.views import ManageUserView
+from profiles import views
+from profiles.api.serializers import AccountSerializer
 
 app_name = "profiles"
 
 urlpatterns = [
     path("<str:username>/", views.ProfileDetailView.as_view(), name="detail"),
     path("<str:username>/follow/", views.FollowView.as_view(), name="follow"),
-    path("<str:username>/manage/", views.ManageUserView.as_view(), name="manage")
+    # path("<str:username>/manage/", views.ManageUserView.as_view(), name="manage"),
+    # path("api/<pk>/account/", views.AccountDetail.as_view(), name="account"),
+    # path("api/<str:username>/account/edit/", views.AccountEditView.as_view(), name="edit"),
+    path("api/<pk>/account/edit/", views.AccountInfoUpdateView.as_view(), name="edit"),
+    # path("api/create/", views.AccountCreate.as_view(), name="create"),
     # path("user/add/", AccountCreateView.as_view(), name="user-add"),
     # path(">str:username>/<int:pk>/", AccountUpdateView.as_view(), name="user-update"),
     # path(">str:username>/<int:pk>/delete", AccountDeleteView.as_view(), name="user-delete"),
--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,24 @@
 from django.contrib.auth.models import User
 from django.http.response import HttpResponseBadRequest
+from django.http import Http404, request
 from django.views.generic import DetailView, View
 from django.views.generic.edit import UpdateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.http import JsonResponse, HttpResponseBadRequest
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponse
 from django.urls import reverse_lazy
 
 from feed.models import Post
 from followers.models import Follower
 # from profiles.forms import AccountForm
 from profiles.models import User, create_user_profile
+
+from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from rest_framework import status, mixins, generics
+from rest_framework.response import Response
+from profiles.models import Profile
+from profiles.api.serializers import AccountSerializer
 
 
 '''
@@ -25,8 +33,9 @@ class ProfileDetailView(DetailView):
     slug_url_kwarg = "username"
 
     def dispatch(self, request, *args, **kwargs):
-        self.request = request
-        return super().dispatch(request, *args, **kwargs)
+        if self.request.user.is_authenticated:
+            self.request = request
+            return super().dispatch(request, *args, **kwargs)
     
     def get_context_data(self, **kwargs):
         user = self.get_object()
@@ -37,6 +46,35 @@ class ProfileDetailView(DetailView):
         if self.request.user.is_authenticated:
             context["you_follow"] = Follower.objects.filter(following=user, followed_by=self.request.user).exists()
         return context
+
+# Adapted from https://www.django-rest-framework.org/tutorial/3-class-based-views/
+# TODO: display in api form but no with correct information or actions (and get 404/405):
+# class AccountDetail(generics.RetrieveUpdateDestroyAPIView):
+#     """
+#     Retrieve, update or delete account information.
+#     """
+#     queryset = Profile.objects.all()
+#     serializer_class = AccountSerializer
+
+
+# class AccountCreate(generics.CreateAPIView):
+#     serializer_class = AccountSerializer
+
+
+class AccountInfoUpdateView(generics.UpdateAPIView):
+    serializer_class = AccountSerializer
+    queryset = Profile.objects.all()
+    
+    def put(self, request, *args, **kwargs):
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
 
 
 '''
@@ -84,46 +122,76 @@ class FollowView(LoginRequiredMixin, View):
 '''
 GET - display account information (only if logged in as that specific user)
 '''
-class AccountView(DetailView):
-    pass
+# class AccountView(LoginRequiredMixin, AccountSerializer, DetailView):
+#     http_method_names = ["get"]
+#     template_name = "profiles/manageprofile.html"
+#     model = User
+#     context_object_name = "user"
+#     slug_field = "username"
+#     slug_url_kwarg = "username"
+#     # profiles = Profile.objects.
+#     # serializer = AccountSerializer(profiles, many=True)
 
+#     def dispatch(self, request, *args, **kwargs):
+#             self.request = request
+#             return super().dispatch(request, *args, **kwargs)
+
+    # def get(self, request: "get", *args, **kwargs) -> HttpResponse:
+    #     return JsonResponse(self.serializer.data, safe=False)
+
+# TODO: MAKE THIS WORK - this goes to the correct "/edit" url but returns a 405
+# class AccountEditView(LoginRequiredMixin, UpdateView):
+#     http_method_names = ["put"]
+
+
+#     def post(self, request: "post", *args, **kwargs) -> HttpResponse:
+#         try:
+#             profile = Profile.objects.get(self.request.user)
+#         except Profile.DoesNotExist:
+#             return HttpResponse(status=404)
+#         data = JSONParser().parse(request)
+#         serializer = AccountSerializer(profile, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)
 
 '''
 TODO: Change to POST method to update account only (change view name too - UpdateAccountView)
 
 API View
 '''
-class ManageUserView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
-    http_method_names = ["get", "post"]
-    template_name = "profiles/manageprofile.html"
-    model = User
-    context_object_name = "user"
-    slug_field = "username"
-    slug_url_kwarg = "username"
-    success_url = "../../{username}/profile"
-    success_message = "Your profile has been successfully updated"
-    fields = ["username", "first_name", "last_name", "email", 'password']
+# class ManageUserView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+#     http_method_names = ["get", "post"]
+#     template_name = "profiles/manageprofile.html"
+#     model = User
+#     context_object_name = "user"
+#     slug_field = "username"
+#     slug_url_kwarg = "username"
+#     success_url = "../../{username}/profile"
+#     success_message = "Your profile has been successfully updated"
+#     fields = ["username", "first_name", "last_name", "email", 'password']
 
-    def dispatch(self, request, *args, **kwargs):
-        self.request = request
-        return super().dispatch(self.request, *args, **kwargs)
+#     def dispatch(self, request, *args, **kwargs):
+#         self.request = request
+#         return super().dispatch(self.request, *args, **kwargs)
 
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        obj = form.save(commit=False)
-        form.send_email()
-        form.instance.created_by = self.request.user
-        return super().form_valid(form)
+#     def form_valid(self, form):
+#         # This method is called when valid form data has been POSTed.
+#         # It should return an HttpResponse.
+#         obj = form.save(commit=False)
+#         form.send_email()
+#         form.instance.created_by = self.request.user
+#         return super().form_valid(form)
 
-    def get_success_message(self, cleaned_data):
-        return self.get_success_message % dict(
-            cleaned_data,
-            calculated_field=self.object.calculated_field,
-        )
+#     def get_success_message(self, cleaned_data):
+#         return self.get_success_message % dict(
+#             cleaned_data,
+#             calculated_field=self.object.calculated_field,
+#         )
 
-    def get_success_url(self):
-        return reverse_lazy("detail", kwargs={"username": self.request.user.username})
+#     def get_success_url(self):
+#         return reverse_lazy("detail", kwargs={"username": self.request.user.username})
 
 
 '''
