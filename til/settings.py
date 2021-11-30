38727efc899928c872ccbd10de2417375203c1f8
Ben Lenton <benlenton@gmail.com>
2021-11-30 15:39:55 +0000

    deleted unecessary files, added hidden secret_key

--- til/settings.py
+++ til/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +27,7 @@ MEDIA_URL = "/media/"
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5vydy9rqmn82hr*-q^f=1e_^*&cfh8%wh_4yixhqyab5-&hjj#'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
